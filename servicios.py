90df55ea56b068b94af2258001657133c7746b04
code_good_master <karendayanaangaritaescobar12@gmail.com>
2026-04-30 16:53:41 -0500

    correccion de errrores

--- servicios.py
+++ servicios.py
@@ -2,16 +2,16 @@ import csv
 def eliminar_servicio():
     archivo = "Servicio.csv"
     nombre_eliminar = input("Nombre del servicio a eliminar: ")
-    
+    encabezado = ["servicio","precio","evento","duracion"]
     filas = []
    
     with open(archivo, "r") as f:
         lector = csv.DictReader(f)
-        filas = [fila for fila in lector if fila["Servicios disponibles"] != nombre_eliminar]
+        filas = [fila for fila in lector if fila["Servicio"] != nombre_eliminar]
 
     
     with open(archivo, "w", newline="") as f:
-        escritor = csv.DictWriter(f, fieldnames=["Servicios disponibles"])
+        escritor = csv.DictWriter(f, fieldnames=encabezado)
         escritor.writeheader()
         escritor.writerows(filas)
     print(f"Si existía el servicio '{nombre_eliminar}', ha sido eliminado.")
