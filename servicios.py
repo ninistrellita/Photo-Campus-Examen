1d88e4ad02351b4710bb21d4c2fc563a8f1e6895
code_good_master <karendayanaangaritaescobar12@gmail.com>
2026-04-30 15:38:30 -0500

    actualizacion de codigo en base a lo indicado

--- servicios.py
+++ servicios.py
@@ -1,4 +1,16 @@
-def eliminar_servicio(id):
-    global servicios
-    servicios = [s for s in servicios if s['id'] != id]
-    print(f"Servicio {id} eliminado.")
+def eliminar_servicio():
+    archivo = "Servicio.csv"
+    nombre_eliminar = input("Nombre del servicio a eliminar: ")
+    
+    filas = []
+   
+    with open(archivo, "r") as f:
+        lector = csv.DictReader(f)
+        filas = [fila for fila in lector if fila["Servicios disponibles"] != nombre_eliminar]
+
+    
+    with open(archivo, "w", newline="") as f:
+        escritor = csv.DictWriter(f, fieldnames=["Servicios disponibles"])
+        escritor.writeheader()
+        escritor.writerows(filas)
+    print(f"Si existía el servicio '{nombre_eliminar}', ha sido eliminado.")
